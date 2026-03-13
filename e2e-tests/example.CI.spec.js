2226c9e24112fbedfab251f119491fc8944312dd
prugit <58348175+prugit@users.noreply.github.com>
2026-03-13 18:44:21 -0500

    Update trademark text in Pokedex test

--- e2e-tests/example.CI.spec.js
+++ e2e-tests/example.CI.spec.js
@@ -4,7 +4,7 @@ describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
     await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
-    await expect(page.getByText('character names are trademarks of Nintendo.')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
 
