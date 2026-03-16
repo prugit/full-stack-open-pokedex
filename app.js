da21b9b0339f924f261a91023b9bb0f81843fc1e
abr-code <example@example.com>
2026-03-16 15:45:08 -0500

    test branch protection on error

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
+  throw new('error')
   res.send('9') // change this string to ensure a new version deployed
 })
 
